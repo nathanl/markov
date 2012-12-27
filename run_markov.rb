80e5813a3923ef3e64d79ca46f8f0dd36235f8c4
Nathan Long <nathan.long@tma1.com>
2012-12-27 16:58:10 -0500

    Bit of reorganization

--- run_markov.rb
+++ run_markov.rb
@@ -3,7 +3,7 @@ require 'markov'
 
 text = File.read('const.txt')
 
-m = Markov.new(text)
-m.populate_text_profile(5)
+m = Markov::Chars.new(text)
+m.populate_input_sequences(5)
 # m.see_phrases_occurring(4)
 puts m.generate(500)
