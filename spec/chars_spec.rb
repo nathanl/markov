17d3fad42548660496c52b8260231dd8acf09ca4
Nathan Long <nathan.long@tma1.com>
2012-12-28 17:55:42 -0500

    First passing test

--- spec/chars_spec.rb
+++ spec/chars_spec.rb
@@ -4,8 +4,13 @@ require 'markov/chars'
 srand(1)
 
 describe Markov::Chars do
+  let(:input_text) {
+    File.read('input_examples/constitution.txt')
+  }
 
   it "should be awesome" do
-    expect(true).to eq(false)
+    chars = Markov::Chars.new(input_text)
+    chars.populate_input_sequences(4)
+    expect(chars.generate(50)).to eq('Coin, and Proceed of Vote; the Executives after of')
   end
 end
