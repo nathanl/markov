a1ac86899e8ccb55819af17a66ca784859b0faf5
Nathan Long <nathanmlong@gmail.com>
2012-12-28 18:55:54 -0500

    Make this a bit more interactive

--- run_markov.rb
+++ run_markov.rb
@@ -1,3 +1,4 @@
+$PROGRAM_NAME = 'Markov'
 $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'markov'
 
@@ -6,4 +7,6 @@ text = File.read('const.txt')
 m = Markov.new(text)
 m.populate_text_profile(5)
 # m.see_phrases_occurring(4)
-puts m.generate(500)
+puts "how much to generate? Give me a number."
+length = $stdin.gets.strip.to_i
+puts m.generate(length)
