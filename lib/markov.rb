898e04f15f5871cd5d66302b2d9e274ddf4cfc4d
Nathan Long <nathanmlong@gmail.com>
2012-12-29 11:18:47 -0500

    Update requires

--- lib/markov.rb
+++ lib/markov.rb
@@ -1,5 +1,6 @@
 # TODO - break into smaller classes to allow for splitting by words, taking
 # non-text input, having different strategies for performance, etc
 require 'pp'
-require 'markov/chars'
 require 'markov/base'
+require 'markov/chars'
+require 'markov/words'
